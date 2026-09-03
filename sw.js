7deaa16071632b96b87c5351d62756bd73d21264
junnosuke0923 <251002352+junnosuke0923@users.noreply.github.com>
2026-09-03 13:22:38 +0900

    deploy 2026-09-03 13:22

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'yojaku-2026-09-03 00:23'
-const FILES = ["./","./index.html","./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png","./sample-photo.jpg","./assets/index-DigwoX18.js","./assets/index-CoXh5euC.css"]
+const CACHE = 'yojaku-2026-09-03 13:22'
+const FILES = ["./","./index.html","./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png","./sample-photo.jpg","./assets/index-BUFW_muu.js","./assets/index-CoXh5euC.css"]
 /*
   覚えたものを探すときの、ゆるめかた。
   ignoreVary が要る。配信の仕方によっては Vary という但し書きが付いていて、
